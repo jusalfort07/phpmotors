4aac61ba292f7830319a2fe0b315ec2c878a4410
Alfred Ortega <jusalfort07@byui.edu>
2022-10-06 21:20:38 +0800

    fix(e-3): Fixed html and css errors

--- view/register.php
+++ view/register.php
@@ -27,38 +27,38 @@
 
             <form action="POST">
                 <div class="first-name form-div">
-                    <label for="first-name">
+                    <label for="clientFirstName">
                         First Name
                     </label>
 
-                    <input type="text" name="clientFirstName" id="clientFirstName" require>
+                    <input type="text" name="clientFirstName" id="clientFirstName">
                 </div>
 
                 <div class="last-name form-div">
-                    <label for="last-name">
+                    <label for="clientLastName">
                         Last Name
                     </label>
 
-                    <input type="text" name="clientLastName" id="clientLastName" require>
+                    <input type="text" name="clientLastName" id="clientLastName">
                 </div>
 
-                <div class="email form-div"">
-                    <label for="email">
+                <div class="email form-div">
+                    <label for="clientEmail">
                         Email
                     </label>
 
-                    <input type="text" name="clientEmail" id="clientEmail" require>
+                    <input type="text" name="clientEmail" id="clientEmail">
                 </div>
 
                 <div class="password form-div">
                     <p>Passwords must be atleast 8 characters and contains at least 1 number,
                          1 capital letter, and 1 special character</p>
                          
-                    <label for="password">
+                    <label for="clientPassword">
                         Password
                     </label>
 
-                    <input type="password" name="clientPassword" id="clientPassword" require>
+                    <input type="password" name="clientPassword" id="clientPassword">
                     
                     <button type="button">
                         Show Password
